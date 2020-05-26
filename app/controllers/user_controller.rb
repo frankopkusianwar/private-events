611c47f1010cdc2e28762a6966ad5ef35dd00a5b
frankopkusianwar <okirorfrank3@gmail.com>
2020-05-26 12:04:38 +0300

    fix UI design

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,5 @@
 class UserController < ApplicationController
   def show
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get 'user/show'
   root 'welcome#index'
   devise_for :users
+  resources :users, only: [:show]
 end

dc35f61f0b70135257b5a41938d2e72c92372990
Expedito Andrade <expeditojazz@gmail.com>
2020-05-26 07:16:56 -0300

    added invitation model and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :creates, foreign_key: 'user_id'
   has_many :events_created, through: :creates, source: :event
-  has_many :users_invited, class_name: 'Invitation', foreign_key: 'invitor_id'
-  has_many :users_invitors, class_name: 'Invitation', foreign_key: 'invitee_id'
-  has_many :invites_received, through: :users_invitors, source: :invitee
+  has_many :users_invited, class_name: 'Invitation', foreign_key: 'invitee_id'
+  has_many :users_invitors, class_name: 'Invitation', foreign_key: 'invitor_id'
+  has_many :invitations, through: :users_invited, source: :invitor
+  has_many :invites, through: :users_invitors, source: :invitee
+  
 
 end
